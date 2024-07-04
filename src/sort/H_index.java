dffee191d0260118f3cf2415fcefc5a6e6e4675c
hyori <gyfl5542@naver.com>
2024-07-04 15:42:24 +0900

    h-index

--- src/sort/H_index.java
+++ src/sort/H_index.java
@@ -1,3 +1,17 @@
+/*
+이 문제의 포인트는 주어진 논문 인용횟수 말고도 그 사이의 값들도 h-index가 될 수 있음을 간파하는 것임.
+ex) [0, 1, 3, 5, 6, 7, 8]
+이 경우 H-index는 4임.
+
+그러니까 주어진 논문 인용횟수를 기준으로 하되, 그 사이사이 값들도 정확하게 체크하기.
+
+ */
+
+
+
+
+
+
 package sort;
 
 import java.util.*;
@@ -8,18 +22,59 @@ public class H_index {
         System.out.println(Solution.solution(new int[]{0, 2, 4, 5, 5}));
     }
 
-    class Solution{
-        public static int solution(int[] citations) {
 
+    class Solution {
+        public int solution(int[] citations) {
+
+            // 일일이 다 확인하면 시간 초과
+            // 정렬해서 인덱스로 비교하면 몇번 이상 인용된게 몇개인지 바로 알 수 있음.
             Arrays.sort(citations);
 
-            for (int idx = citations.length - 1; idx >= 0; idx--) {
-                if (citations[idx] > citations.length) continue;
+            int hIndex = 0;
 
-                if(citations.length - idx >= citations[idx]) return citations[idx];
+
+            // 모든 논문 인용횟수를 기준으로 확인하되 그 사이 값들도 꼼꼼하게 전부 확인하기
+            for (int idx =0 ; idx < citations.length; idx++) {
+
+
+                // 첫번째 값보다 적은 경우 에크
+                if (idx == 0) {
+                    for (int subIdx = 0; subIdx < citations[0]; subIdx++) {
+                        if (citations.length >= subIdx){
+                            hIndex = subIdx;
+                        }
+
+                    }
+                }
+
+                // 현재 논문인용 횟수보다 큰 인용횟수를 비교해서 그만할지 정하기.
+                if (citations.length-idx >= citations[idx]){
+
+
+                    hIndex = citations[idx];
+
+                    // 마지막 인용횟수라면 뒤 인덱스와 비교하지 않으므로 끝내기
+                    if (idx == citations.length-1){
+                        break;
+                    }
+
+                    // 다음 주어진 인용 횟수 사이의 값들 확인하기
+                    for (int subIdx = 1; subIdx < citations[idx+1]-citations[idx]; subIdx++)
+                        // 현재 인덱스의 논문은 포함하지 않으므로 인용 횟수 이상 논문 하나 빼주기
+                        if (citations.length-idx-1 >= citations[idx]+subIdx){
+                            hIndex = citations[idx]+subIdx;
+                        } else {
+                            // 사실 내부 for문 아니고 외부 for문 break 하는 게 맞음.
+                            break;
+                        }
+
+                } else
+                    break;
             }
 
-            return 0;
+            return hIndex;
+
+
         }
     }
 }
--- src/sort/Test.java
+++ src/sort/Test.java
@@ -6,8 +6,13 @@ public class Test {
 
     public static void main(String[] args) {
         int[] arr = new int[] {1, 2};
+        String[] strArr = new String[]{"a", "c", "b"};
 
-
+        Arrays.sort(strArr, (o1, o2) -> o2.compareTo(o1));
+        System.out.println("숫자 " + "106".compareTo("610"));
+        System.out.println(Arrays.toString(strArr));
+        System.out.println("a".compareTo("b"));
+        System.out.println("b".compareTo("a"));
 
         List<Integer> list = new ArrayList<>();
 
